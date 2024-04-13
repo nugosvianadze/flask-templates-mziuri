b2e56bf1ac6e67ffe7c1cce2c7d796d5ff3280ba
nugosvianadze <nugzar.svianadze.1@btu.edu.ge>
2024-04-13 14:34:25 +0400

    added filters for jinja2

--- app.py
+++ app.py
@@ -1,20 +1,39 @@
 from flask import Flask, render_template
+from jinja2 import Environment, PackageLoader, select_autoescape
 
 app = Flask(__name__)
+env = Environment(
+    loader=PackageLoader("flask-templates-mziuri"),
+    autoescape=select_autoescape()
+)
 
+def remove_spaces(value:str):
+    return value.replace(' ', '')
+
+
+def square(value):
+    return int(value)**2
+
+app.jinja_env.filters['remove_spaces'] = remove_spaces
+app.jinja_env.filters['square'] = square
 
 @app.route('/')
 @app.route('/home')
 def home():
     first_name, last_name = 'Nugo', 'Svianadze'
     title = 'Home Page'
+    my_num = 25
     return render_template('index.html', first_name=first_name,
                            last_name=last_name, title=title)
 
 
 @app.route('/features')
 def features():
-    return render_template('features.html')
+    title = 'fe atu r e s s'
+    items = [1, 2, 3, 4, 5]
+    my_num = 25
+    return render_template('features.html', title=title, items=items,
+                           my_num=my_num)
 
 
 if __name__ == '__main__':
